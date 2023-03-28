f7c44623f20af0e8991b1d9379895b3699ca35cb
daniel.zhang <daniel.zhang@fulicare.com>
2023-03-28 18:39:44 +0800

    修改D_BITS值

--- minilzo.c
+++ minilzo.c
@@ -4618,7 +4618,7 @@ int __far __pascal LibMain ( int a, short b, short c, long d )
 
 #define LZO_NEED_DICT_H 1
 #ifndef D_BITS
-#define D_BITS          14
+#define D_BITS          11
 #endif
 #define D_INDEX1(d,p)       d = DM(DMUL(0x21,DX3(p,5,5,6)) >> 5)
 #define D_INDEX2(d,p)       d = (d & (D_MASK & 0x7ff)) ^ (D_HIGH | 0x1f)
